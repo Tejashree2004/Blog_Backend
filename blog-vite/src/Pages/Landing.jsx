4900b350be03a78593538d74715e60300d7dcb55
Tejashree2004 <tejashreeshejwal264@gmail.com>
2026-03-26 17:33:32 +0530

    fixed duplicate create blog button issue

--- blog-vite/src/Pages/Landing.jsx
+++ blog-vite/src/Pages/Landing.jsx
@@ -16,22 +16,7 @@ function Landing({ blogs, deleteBlog, savedBlogIds, saveBlog, unsaveBlog }) {
 
   return (
     <div className="page-wrapper">
-      <div style={{ margin: "20px 0", textAlign: "center" }}>
-        <button
-          onClick={handleCreateBlog}
-          style={{
-            padding: "10px 20px",
-            backgroundColor: "#3b82f6",
-            color: "#fff",
-            border: "none",
-            borderRadius: "6px",
-            cursor: "pointer",
-            fontSize: "16px"
-          }}
-        >
-          Create Blog
-        </button>
-      </div>
+    
 
       <Blog
         blogs={blogs}
